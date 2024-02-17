dfa9187180b687b8bf190bb5800dcb488c0c2ccf
Emil Gaarde <gaardee@gmail.com>
2024-02-17 19:11:41 +0100

    Create fake spin button

--- src/index.js
+++ src/index.js
@@ -10,49 +10,20 @@ var currentWheelRotation = 0;
 var currentWheelIndex = 0;
 var isRotating = false;
 const rouletteWheelNumbers = [
-  0,
-  32,
-  15,
-  19,
-  4,
-  21,
-  2,
-  25,
-  17,
-  34,
-  6,
-  27,
-  13,
-  36,
-  11,
-  30,
-  8,
-  23,
-  10,
-  5,
-  24,
-  16,
-  33,
-  1,
-  20,
-  14,
-  31,
-  9,
-  22,
-  18,
-  29,
-  7,
-  28,
-  12,
-  35,
-  3,
-  26
+  0, 32, 15, 19, 4, 21, 2, 25, 17, 34, 6, 27, 13, 36, 11, 30, 8, 23, 10, 5, 24,
+  16, 33, 1, 20, 14, 31, 9, 22, 18, 29, 7, 28, 12, 35, 3, 26,
+];
+const rouletteWheelRedNumbers = [
+  32, 19, 21, 25, 34, 27, 36, 30, 23, 5, 16, 1, 14, 9, 18, 7, 12, 3,
+];
+const rouletteWheelBlackNumbers = [
+  15, 4, 2, 17, 6, 13, 11, 8, 10, 24, 33, 20, 31, 22, 29, 28, 35, 26,
 ];
 
-const getRouletteWheelNumber = index =>
+const getRouletteWheelNumber = (index) =>
   rouletteWheelNumbers[index >= 0 ? index % 37 : 37 - Math.abs(index % 37)];
 
-const getRouletteWheelColor = index => {
+const getRouletteWheelColor = (index) => {
   const i = index >= 0 ? index % 37 : 37 - Math.abs(index % 37);
   return i == 0 ? "green" : i % 2 == 0 ? "black" : "red";
 };
@@ -60,7 +31,7 @@ const getRouletteWheelColor = index => {
 window.rouletteWheelNumbers = rouletteWheelNumbers;
 
 function addFlipper() {
-  const mkDiv = className => {
+  const mkDiv = (className) => {
     const d = document.createElement("div");
     d.classList.add(...className.split(" "));
     return d;
@@ -70,14 +41,19 @@ function addFlipper() {
   const back = mkDiv("back-face");
   flipper.appendChild(front);
   flipper.appendChild(back);
-  document.querySelector(".result").appendChild(flipper);
+  const resultBox = document.querySelector(".result");
+  resultBox.insertBefore(flipper, resultBox.firstChild);
   return (number, color) => {
     flipper.classList.add("flip", color);
     back.innerText = number;
   };
 }
 
-function startRotation(speed) {
+function startRotation(speed, pickedNumber = null) {
+  if (pickedNumber) {
+    speed =
+      currentWheelIndex + 37 * 3 - rouletteWheelNumbers.indexOf(pickedNumber);
+  }
   if (isRotating) {
     return;
   }
@@ -96,10 +72,10 @@ function startRotation(speed) {
     console.log(getRouletteWheelNumber(currentWheelIndex), "---> ", result);
     var myAnimation = anime({
       targets: [".layer-2", ".layer-4"],
-      rotate: function() {
+      rotate: function () {
         return newRotaion;
       },
-      duration: function() {
+      duration: function () {
         return 5000;
       },
       loop: 1,
@@ -108,8 +84,8 @@ function startRotation(speed) {
       // easing: "cubicBezier(0.000, 1.175, 0.980, 0.990)",
       complete: (...args) => {
         currentWheelRotation = newRotaion;
-        currentWheelIndex = newWheelIndex;
-      }
+        currentWheelIndex = newWheelIndex % 37;
+      },
     });
   })();
 
@@ -122,10 +98,10 @@ function startRotation(speed) {
         { value: 0, duration: 2000 },
         { value: 20, duration: 1000 },
         { value: 25, duration: 900 },
-        { value: 50, duration: 1000 }
+        { value: 50, duration: 1000 },
       ],
       rotate: [{ value: newRotaion, duration: 4000 }],
-      duration: function() {
+      duration: function () {
         return 4000; // anime.random(800, 1400);
       },
       loop: 1,
@@ -134,7 +110,7 @@ function startRotation(speed) {
         currentBallRotation = newRotaion;
         writeResult(result, resultColor);
         isRotating = false;
-      }
+      },
     });
   })();
 }
@@ -147,7 +123,7 @@ function offsetEl(el) {
     top: rect.top + scrollTop,
     left: rect.left + scrollLeft,
     width: rect.width,
-    height: rect.height
+    height: rect.height,
   };
 }
 
@@ -168,54 +144,26 @@ function isInRadiusEl(el, x, y) {
   return Math.pow(dx, 2) + Math.pow(dy, 2) <= Math.pow(r, 2);
 }
 
-const documentEvent = eventName =>
-  pipe(
-    fromEvent(document, eventName),
-    map(e =>
-      e.type == "touchstart" || e.type == "touchmove"
-        ? { x: e.touches[0].clientX, y: e.touches[0].clientY }
-        : { x: e.clientX, y: e.clientY }
-    )
-  );
-
-const tryRotate = ([p0, p1]) => {
-  const w = document.querySelector(".layer-2.wheel");
-  if (isInRadiusEl(w, p0.x, p0.y)) {
-    const d = Math.round(
-      Math.sqrt(Math.pow(p0.x - p1.x, 2) + Math.pow(p0.y - p1.y, 2)) / 4
-    );
-    if (Math.abs(d) > 3) {
-      window.startRotation(d);
-    }
-  }
+document.getElementById("spin").onclick = function () {
+  window.startRotation(100);
 };
 
-zip(documentEvent("mousedown"))(documentEvent("mouseup")).subscribe({
-  next: tryRotate
-});
+document.getElementById("spinRed").onclick = function () {
+  window.startRotation(
+    100,
+    rouletteWheelRedNumbers.sort((a, b) => 0.5 - Math.random())[
+      Math.floor(Math.random() * rouletteWheelRedNumbers.length)
+    ]
+  );
+};
 
-zip(documentEvent("touchstart"))(
-  pipe(
-    withLatestFrom(documentEvent("touchmove"))(fromEvent(document, "touchend")),
-    map(([_, r]) => r)
-  )
-).subscribe({
-  next: tryRotate
-});
-
-pipe(
-  withLatestFrom(documentEvent("touchmove"))(fromEvent(document, "touchend")),
-  map(([_, r]) => r)
-).subscribe({
-  next: e => console.log(e)
-});
-
-document.querySelector(".roulette-wheel").addEventListener(
-  "touchmove",
-  e => {
-    e.preventDefault();
-  },
-  { passive: false }
-);
+document.getElementById("spinBlack").onclick = function () {
+  window.startRotation(
+    100,
+    rouletteWheelBlackNumbers.sort((a, b) => 0.5 - Math.random())[
+      Math.floor(Math.random() * rouletteWheelBlackNumbers.length)
+    ]
+  );
+};
 
 window.startRotation = startRotation;
